077428ffb8b52e459e5b134b4206882650fd98e9
teymuur <teymur_babayev08@yahoo.com>
2023-11-24 01:23:16 +0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 
 class ImageViewer(tk.Tk):
     def __init__(self):
@@ -9,70 +9,81 @@ class ImageViewer(tk.Tk):
         self.title("Image Viewer")
         self.geometry("800x600")
 
-        self.image_label = tk.Label(self)
-        self.image_label.pack(expand="true")
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(expand="true")
 
         menu_bar = tk.Menu(self)
         self.config(menu=menu_bar)
-        #file menu
+        # file menu
         file_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="File", menu=file_menu)
         file_menu.add_command(label="Open", command=self.open_image)
         file_menu.add_command(label="Save", command=self.save_image)
         file_menu.add_command(label="Exit", command=self.quit)
 
-        #edit menu
+        # edit menu
         edit_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Edit", menu=edit_menu)
         edit_menu.add_command(label="Rotate Right", command=self.rotate_right)
         edit_menu.add_command(label="Rotate Left", command=self.rotate_left)
-        edit_menu.add_command(label="Crop", command=self.crop_image)
-
-        #help menu
+        edit_menu.add_command(label="Crop", command=self.start_crop)
+        edit_menu.add_command(label="Undo", command=self.undo, accelerator="Ctrl+Z")
+        edit_menu.add_command(label="Redo", command=self.redo, accelerator="Ctrl+Y")
+        
+        # help menu
         help_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Help", menu=help_menu)
         help_menu.add_command(label="About", command=self.show_about)
         help_menu.add_command(label="Keyboard Shortcuts", command=self.show_shortcuts)
+        
         self.rotation_count = 0
         self.original_image = None
+        self.image_history = []
+        self.history_index = -1
 
-        #Keyboard shortcuts
-        self.bind_all("<Control-o>", lambda event: self.open_image())
-        self.bind_all("<Control-s>", lambda event: self.save_image())
-        self.bind_all("<Alt-F4>", lambda event: self.quit())
-        self.bind_all("<Control-Shift-R>", lambda event: self.rotate_right())
-        self.bind_all("<Control-R>", lambda event: self.rotate_left())
-        
-        #cropping adjustments
+        # Keyboard shortcuts
+        self.bind_all("<Control-o>", self.open_image)
+        self.bind_all("<Control-s>", self.save_image)
+        self.bind_all("<Alt-F4>", self.quit)
+        self.bind_all("<Control-Shift-R>", self.rotate_right)
+        self.bind_all("<Control-R>", self.rotate_left)
+        self.bind_all("<Control-z>", self.undo)
+        self.bind_all("<Control-y>", self.redo)
+
+        # cropping adjustments
         self.cropping = False
         self.crop_start = (0, 0)
         self.crop_rectangle = None
 
-    def open_image(self):
+    def open_image(self, event=None):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
         if file_path:
             image = Image.open(file_path)
             self.original_image = image.copy()
             self.rotation_count = 0
             self.update_image()
+            self.save_to_history()
 
     def update_image(self):
         if hasattr(self, 'original_image'):
             rotated_image = self.original_image.rotate(90 * self.rotation_count)
             photo = ImageTk.PhotoImage(rotated_image)
 
-            self.image_label.config(image=photo)
-            self.image_label.image = photo
+            self.canvas.config(width=photo.width(), height=photo.height())
+            self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+            self.canvas.photo = photo
 
-    def rotate_right(self):
+    def rotate_right(self, event=None):
         self.rotation_count -= 1
         self.update_image()
+        self.save_to_history()
 
-    def rotate_left(self):
+    def rotate_left(self, event=None):
         self.rotation_count += 1
         self.update_image()
+        self.save_to_history()
 
-    def save_image(self):
+    def save_image(self, event=None):
         if hasattr(self, 'original_image'):
             file_types = [
                 ("PNG files", "*.png"),
@@ -89,36 +100,7 @@ class ImageViewer(tk.Tk):
             if file_path:
                 rotated_image = self.original_image.rotate(90 * self.rotation_count)
                 rotated_image.save(file_path)
-    def crop_image(self):
-        if hasattr(self, 'original_image'):
-            self.cropping = not self.cropping
-
-            if self.cropping:
-                messagebox.showinfo("Crop", "Click and drag to select a region for cropping.")
-            else:
-                self.apply_crop()
-
-    def apply_crop(self, event=None):
-        if self.cropping and self.crop_start:
-            x1, y1, x2, y2 = self.crop_start[0], self.crop_start[1], event.x, event.y
-
-            # Convert coordinates to rotated image coordinates
-            rotated_image = self.original_image.rotate(90 * self.rotation_count)
-            width, height = rotated_image.size
-            x1, y1 = min(x1, x2), min(y1, y2)
-            x2, y2 = max(x1, x2), max(y1, y2)
-
-            x1, y1 = int(x1 * width / self.image_label.winfo_width()), int(y1 * height / self.image_label.winfo_height())
-            x2, y2 = int(x2 * width / self.image_label.winfo_width()), int(y2 * height / self.image_label.winfo_height())
-
-            cropped_image = rotated_image.crop((x1, y1, x2, y2))
-            self.original_image = cropped_image
-            self.rotation_count = 0
-            self.cropping = False
-            self.crop_start = None
-            self.crop_rectangle = None
-            self.update_image()
-
+                self.save_to_history()
 
     def show_shortcuts(self):
         shortcuts_info = (
@@ -126,7 +108,9 @@ class ImageViewer(tk.Tk):
             "Ctrl+S - Save Image\n"
             "Alt+F4 - Exit\n"
             "Ctrl+Shift+R - Rotate Right\n"
-            "Ctrl+R - Rotate Left"
+            "Ctrl+R - Rotate Left\n"
+            "Ctrl+Z - Undo\n"
+            "Ctrl+Y - Redo"
         )
         messagebox.showinfo("Keyboard Shortcuts", shortcuts_info)
 
@@ -138,6 +122,66 @@ class ImageViewer(tk.Tk):
             "© 2023 Teymur Babayev"
         )
         messagebox.showinfo("About", about_info)
+
+    def start_crop(self):
+        if hasattr(self, 'original_image'):
+            self.cropping = True
+            self.canvas.bind("<Button-1>", self.crop_start_position)
+            self.canvas.bind("<B1-Motion>", self.crop_drag)
+            self.canvas.bind("<ButtonRelease-1>", self.crop_end)
+
+    def crop_start_position(self, event):
+        self.crop_start = (event.x, event.y)
+
+    def crop_drag(self, event):
+        if self.crop_rectangle:
+            self.canvas.delete(self.crop_rectangle)
+
+        x, y = self.crop_start
+        x1, y1 = (event.x, event.y)
+
+        self.crop_rectangle = self.canvas.create_rectangle(x, y, x1, y1, outline="red", dash=(2, 2))
+
+    def crop_end(self, event):
+        if self.crop_rectangle:
+            self.canvas.delete(self.crop_rectangle)
+            x, y = self.crop_start
+            x1, y1 = (event.x, event.y)
+
+            rotated_image = self.original_image.rotate(90 * self.rotation_count)
+            cropped_image = rotated_image.crop((x, y, x1, y1))
+            self.original_image = cropped_image
+            self.cropping = False
+            self.canvas.unbind("<Button-1>")
+            self.canvas.unbind("<B1-Motion>")
+            self.canvas.unbind("<ButtonRelease-1>")
+            self.save_to_history()
+
+    def save_to_history(self):
+        if self.original_image is not None:
+            # If we are undoing, discard any redo history beyond the current position
+            if self.history_index < len(self.image_history) - 1:
+                self.image_history = self.image_history[:self.history_index + 1]
+
+            # Save a copy of the current image
+            self.image_history.append(self.original_image.copy())
+            self.history_index = len(self.image_history) - 1
+
+    def undo(self, event=None):
+        if self.history_index > 0:
+            self.history_index -= 1
+            self.original_image = self.image_history[self.history_index].rotate(-90 * self.rotation_count)
+            self.update_image()
+
+
+    def redo(self, event=None):
+        if self.history_index < len(self.image_history) - 1:
+            self.history_index += 1
+            self.original_image = self.image_history[self.history_index].rotate(-90 * self.rotation_count)
+            self.update_image()
+
+
+
 if __name__ == "__main__":
     app = ImageViewer()
     app.mainloop()
