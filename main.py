10f9099ccf21a056fe45475a0e4cbaa72f1000e7
teymuur <teymur_babayev08@yahoo.com>
2023-12-10 11:53:34 +0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox,Scale
+from tkinter import filedialog, messagebox, Scale
 from PIL import Image, ImageTk, ImageEnhance
 
-class AdjustmentsWindow(tk.Toplevel):
-    def __init__(self, parent, apply_adjustments):
-        super().__init__(parent)
-        self.title("Adjustments")
-        self.geometry("300x150")
-
-        self.brightness_label = tk.Label(self, text="Brightness")
-        self.brightness_slider = Scale(self, from_=0.1, to=2.0, resolution=0.1, orient=tk.HORIZONTAL)
-        self.brightness_slider.set(1.0)
-
-        self.contrast_label = tk.Label(self, text="Contrast")
-        self.contrast_slider = Scale(self, from_=0.1, to=2.0, resolution=0.1, orient=tk.HORIZONTAL)
-        self.contrast_slider.set(1.0)
-
-        self.apply_button = tk.Button(self, text="Apply", command=apply_adjustments)
-
-        self.brightness_label.grid(row=0, column=0, pady=(10, 0))
-        self.brightness_slider.grid(row=0, column=1, pady=(10, 0))
-        self.contrast_label.grid(row=1, column=0, pady=(10, 0))
-        self.contrast_slider.grid(row=1, column=1, pady=(10, 0))
-        self.apply_button.grid(row=2, column=0, columnspan=2, pady=(10, 0))
 class ImageViewer(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -30,40 +9,13 @@ class ImageViewer(tk.Tk):
         self.title("Image Viewer")
         self.geometry("800x600")
 
+        # Canvas for displaying the image
         self.canvas = tk.Canvas(self)
-        self.canvas.pack(expand="true")
+        self.canvas.pack(expand="true", fill="both")
 
-        menu_bar = tk.Menu(self)
-        self.config(menu=menu_bar)
-        # file menu
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="Open", command=self.open_image, accelerator="Ctrl+N")
-        file_menu.add_command(label="Save", command=self.save_image, accelerator="Ctrl+S")
-        file_menu.add_command(label="Exit", command=self.quit, accelerator="Alt+F4")
-
-        # edit menu
-        edit_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Edit", menu=edit_menu)
-        edit_menu.add_command(label="Rotate Right", command=self.rotate_right,accelerator="Ctrl+Shift+R")
-        edit_menu.add_command(label="Rotate Left", command=self.rotate_left,accelerator="Ctrr+$")
-        edit_menu.add_command(label="Crop", command=self.start_crop)
-        edit_menu.add_command(label="Adjustments", command=self.show_adjustments)  # New option for adjustments
-
-
-        edit_menu.add_command(label="Undo", command=self.undo, accelerator="Ctrl+Z")
-        edit_menu.add_command(label="Redo", command=self.redo, accelerator="Ctrl+Y")
-        
-        # help menu
-        help_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=self.show_about)
-        help_menu.add_command(label="Keyboard Shortcuts", command=self.show_shortcuts)
-        
-
-        # Create a right frame for adjustments
+        # Frame for adjustments on the right side
         self.adjustments_frame = tk.Frame(self)
-        self.adjustments_frame.pack(side=tk.RIGHT, fill=tk.Y, anchor=tk.N)
+        self.adjustments_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=10)
 
         # Add brightness and contrast sliders
         self.brightness_label = tk.Label(self.adjustments_frame, text="Brightness")
@@ -83,23 +35,49 @@ class ImageViewer(tk.Tk):
         self.contrast_slider.pack(pady=(0, 10))
         self.apply_adjustments_button.pack(pady=(10, 0))
 
+        # Menu bar
+        menu_bar = tk.Menu(self)
+        self.config(menu=menu_bar)
 
+        # File menu
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Open", command=self.open_image, accelerator="Ctrl+N")
+        file_menu.add_command(label="Save", command=self.save_image, accelerator="Ctrl+S")
+        file_menu.add_command(label="Exit", command=self.quit, accelerator="Alt+F4")
+
+        # Edit menu
+        edit_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Edit", menu=edit_menu)
+        edit_menu.add_command(label="Rotate Right", command=self.rotate_right, accelerator="Ctrl+Shift+R")
+        edit_menu.add_command(label="Rotate Left", command=self.rotate_left, accelerator="Ctrr+$")
+        edit_menu.add_command(label="Crop", command=self.start_crop)
+        edit_menu.add_command(label="Undo", command=self.undo, accelerator="Ctrl+Z")
+        edit_menu.add_command(label="Redo", command=self.redo, accelerator="Ctrl+Y")
+        edit_menu.add_command(label="Adjustments", command=self.show_adjustments)
+
+        # Help menu
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command=self.show_about)
+        help_menu.add_command(label="Keyboard Shortcuts", command=self.show_shortcuts)
+
+        # Attributes for image manipulation
         self.rotation_count = 0
         self.original_image = None
         self.image_history = []
         self.history_index = -1
 
         # Keyboard shortcuts
-        self.bind_all("<Control-n>",  self.open_image)
-
+        self.bind_all("<Control-n>", self.open_image)
         self.bind_all("<Control-s>", self.save_image)
-        self.bind_all("<Alt-F4>",self.quit)
+        self.bind_all("<Alt-F4>", self.quit)
         self.bind_all("<Control-Shift-r>", self.rotate_right)
         self.bind_all("<Control-r>", self.rotate_left)
         self.bind_all("<Control-z>", self.undo)
         self.bind_all("<Control-y>", self.redo)
 
-        # cropping adjustments
+        # Cropping adjustments
         self.cropping = False
         self.crop_start = (0, 0)
         self.crop_rectangle = None
@@ -158,7 +136,6 @@ class ImageViewer(tk.Tk):
             "Alt+F4 - Exit\n"
             "Ctrl+Shift+R - Rotate Right\n"
             "Ctrl+R - Rotate Left\n"
-
             "Ctrl+Z - Undo\n"
             "Ctrl+Y - Redo"
         )
@@ -190,6 +167,8 @@ class ImageViewer(tk.Tk):
         x, y = self.crop_start
         x1, y1 = (event.x, event.y)
 
+        # ..., your existing code continued
+
         self.crop_rectangle = self.canvas.create_rectangle(x, y, x1, y1, outline="red", dash=(2, 2))
 
     def crop_end(self, event):
@@ -217,28 +196,21 @@ class ImageViewer(tk.Tk):
             # Save a copy of the current image
             self.image_history.append(self.original_image.copy())
             self.history_index = len(self.image_history) - 1
-    def show_adjustments(self):
-        if self.adjustments_window is None:
-            self.adjustments_window = AdjustmentsWindow(self, self.apply_adjustments)
-
-    def apply_adjustments(self):
-        # Apply adjustments to the original image
-        brightness_factor = self.brightness_slider.get()
-        contrast_factor = self.contrast_slider.get()
-
-        enhanced_image = self.original_image.copy()
-        enhanced_image = ImageEnhance.Brightness(enhanced_image).enhance(float(brightness_factor))
-        enhanced_image = ImageEnhance.Contrast(enhanced_image).enhance(float(contrast_factor))
-
-        self.original_image = enhanced_image
-        self.update_image()
-        self.save_to_history()
 
     def display_image(self, image):
         self.displayed_image = ImageTk.PhotoImage(image)
         self.image_label.config(image=self.displayed_image)
         self.image_label.image = self.displayed_image
 
+    def adjust_brightness(self, brightness_factor):
+        if hasattr(self, 'original_image'):
+            enhanced_image = ImageEnhance.Brightness(self.original_image).enhance(float(brightness_factor))
+            self.display_image(enhanced_image)
+
+    def adjust_contrast(self, contrast_factor):
+        if hasattr(self, 'original_image'):
+            enhanced_image = ImageEnhance.Contrast(self.original_image).enhance(float(contrast_factor))
+            self.display_image(enhanced_image)
 
     def undo(self, event=None):
         if self.history_index > 0:
@@ -246,14 +218,11 @@ class ImageViewer(tk.Tk):
             self.original_image = self.image_history[self.history_index].rotate(-90 * self.rotation_count)
             self.update_image()
 
-
     def redo(self, event=None):
         if self.history_index < len(self.image_history) - 1:
             self.history_index += 1
             self.original_image = self.image_history[self.history_index].rotate(-90 * self.rotation_count)
             self.update_image()
-
-
 
 
 if __name__ == "__main__":
